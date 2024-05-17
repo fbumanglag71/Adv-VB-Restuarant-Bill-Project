1f5fbab9148fb69a3b78e3480a88b680bd00e25d
franc <franc@BLUE>
2024-05-16 21:52:43 -0700

    Add project files.

---
+++ frmBill.Designer.vb
@@ -0,0 +1,133 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmBill
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtPizza = New System.Windows.Forms.TextBox()
+        Me.txtFries = New System.Windows.Forms.TextBox()
+        Me.txtDrinks = New System.Windows.Forms.TextBox()
+        Me.btnCompute = New System.Windows.Forms.Button()
+        Me.lstBox = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(61, 46)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(168, 18)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "How many pizza slices?"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(110, 86)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(119, 18)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "How many fries?"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(69, 135)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(160, 18)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "How many soft drinks?"
+        '
+        'txtPizza
+        '
+        Me.txtPizza.Location = New System.Drawing.Point(254, 44)
+        Me.txtPizza.Name = "txtPizza"
+        Me.txtPizza.Size = New System.Drawing.Size(50, 20)
+        Me.txtPizza.TabIndex = 3
+        '
+        'txtFries
+        '
+        Me.txtFries.Location = New System.Drawing.Point(254, 84)
+        Me.txtFries.Name = "txtFries"
+        Me.txtFries.Size = New System.Drawing.Size(50, 20)
+        Me.txtFries.TabIndex = 4
+        '
+        'txtDrinks
+        '
+        Me.txtDrinks.Location = New System.Drawing.Point(254, 133)
+        Me.txtDrinks.Name = "txtDrinks"
+        Me.txtDrinks.Size = New System.Drawing.Size(50, 20)
+        Me.txtDrinks.TabIndex = 5
+        '
+        'btnCompute
+        '
+        Me.btnCompute.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.btnCompute.Font = New System.Drawing.Font("Arial Narrow", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCompute.Location = New System.Drawing.Point(344, 46)
+        Me.btnCompute.Name = "btnCompute"
+        Me.btnCompute.Size = New System.Drawing.Size(118, 111)
+        Me.btnCompute.TabIndex = 6
+        Me.btnCompute.Text = "Compute Total Cost"
+        Me.btnCompute.UseVisualStyleBackColor = False
+        '
+        'lstBox
+        '
+        Me.lstBox.FormattingEnabled = True
+        Me.lstBox.Location = New System.Drawing.Point(64, 202)
+        Me.lstBox.Name = "lstBox"
+        Me.lstBox.Size = New System.Drawing.Size(398, 95)
+        Me.lstBox.TabIndex = 7
+        '
+        'frmBill
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(534, 327)
+        Me.Controls.Add(Me.lstBox)
+        Me.Controls.Add(Me.btnCompute)
+        Me.Controls.Add(Me.txtDrinks)
+        Me.Controls.Add(Me.txtFries)
+        Me.Controls.Add(Me.txtPizza)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "frmBill"
+        Me.Text = "Restuarant Bill"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtPizza As TextBox
+    Friend WithEvents txtFries As TextBox
+    Friend WithEvents txtDrinks As TextBox
+    Friend WithEvents btnCompute As Button
+    Friend WithEvents lstBox As ListBox
+End Class
---
+++ frmBill.vb
@@ -0,0 +1,35 @@
+﻿'' Author: Francisco Bumanglag
+'' Project: Restuarant Bill, Page 236
+'' Assignment: Module 4 Exercise 
+'' Course: Adv Visual Basic, Santa Ana College
+'' Class: CMPR205 Dr. Kimberly Davis 
+'' Date: February 25, 2024
+
+
+Public Class frmBill
+
+    Private Sub btnCompute_Click(sender As Object, e As EventArgs) Handles btnCompute.Click
+
+        ''declare the variables
+        Dim pizza As Integer = txtPizza.Text
+        Dim fries As Integer = txtFries.Text
+        Dim drinks As Integer = txtDrinks.Text
+
+        ''calculations 
+        Dim pizzaPrice As Decimal = pizza * 1.75D
+        Dim friesPrice As Decimal = fries * 2D
+        Dim drinkPrice As Decimal = drinks * 1.25D
+        Dim totalAll As Decimal = pizzaPrice + friesPrice + drinkPrice
+
+        ''clear the listbox 
+        lstBox.Items.Clear()
+
+        ''display the outcome in the listbox
+        lstBox.Items.Add("ITEM                QUANTITY          PRICE")
+        lstBox.Items.Add("Pizza Slices            " & pizza & "                     $" & pizzaPrice)
+        lstBox.Items.Add("Fries                       " & pizza & "                     $" & pizzaPrice)
+        lstBox.Items.Add("Soft Drinks             " & pizza & "                     $" & pizzaPrice)
+        lstBox.Items.Add("TOTAL" & "                                          $" & totalAll)
+
+    End Sub
+End Class
